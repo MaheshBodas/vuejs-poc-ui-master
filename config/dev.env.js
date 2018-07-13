8f3e9e2be659ff8d74394439ff08dd95ad88ba90
Mahesh Bodas <36446088+MaheshBodas@users.noreply.github.com>
2018-07-14 02:23:17 +0530

    Commented BASE_API from config.

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,9 +3,9 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
+  NODE_ENV: '"development"'
   //BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
   // BASE_API: '"http://127.0.0.1:9528"',
   // BASE_API: '"https://britecore-poc-server.herokuapp.com:443"',
-  BASE_API: process.env.BASE_API
+  // BASE_API: process.env.BASE_API
 })
