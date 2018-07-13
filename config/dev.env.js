5fef8ed460e1b8aafbace4ac3707770832382916
Mahesh Bodas <36446088+MaheshBodas@users.noreply.github.com>
2018-07-14 01:45:21 +0530

    Changed BASE_API location as constant from Environment settings file.

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,5 +6,6 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   //BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
   // BASE_API: '"http://127.0.0.1:9528"',
-  BASE_API: '"https://britecore-poc-server.herokuapp.com:443"',
+  // BASE_API: '"https://britecore-poc-server.herokuapp.com:443"',
+  BASE_API: process.env.BASE_API
 })
